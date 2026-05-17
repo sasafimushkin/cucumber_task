33157694e35215c760fc5317e102255c6b92a04b
Oleksandr Fimushkin <dev@example.com>
2026-05-17 19:29:30 +0300

    Update: Add Oleksandr Fimushkin as author and add Firefox browser support

--- wdio.cucumber.conf.js
+++ wdio.cucumber.conf.js
@@ -1,16 +1,31 @@
+const path = require('path');
+
 exports.config = {
     runner: 'local',
     specs: ['./features/**/*.feature'],
     maxInstances: 1,
-    capabilities: [{
-        maxInstances: 1,
-        browserName: 'chrome',
-    }],
+    capabilities: [
+        {
+            maxInstances: 1,
+            browserName: 'chrome',
+            'goog:chromeOptions': {
+                args: ['--disable-blink-features=AutomationControlled'],
+            }
+        },
+        {
+            maxInstances: 1,
+            browserName: 'firefox',
+            'moz:firefoxOptions': {
+                args: [],
+            }
+        }
+    ],
     framework: 'cucumber',
     cucumberOpts: {
         require: ['./features/step-definitions/**/*.js'],
         timeout: 60000,
     },
-    services: ['selenium-standalone'],
+    port: 9515,
+    path: '/',
     reporters: ['spec'],
 };
