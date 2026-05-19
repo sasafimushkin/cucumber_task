9c37b296650821f6eda54ad287ae596f8ce96cb0
anton <sasafimushkin@gmail.com>
2026-05-19 09:01:29 +0300

    commitpc

--- features/step-definitions/login.steps.js
+++ features/step-definitions/login.steps.js
@@ -1,4 +1,4 @@
-const { Given, When, Then } = require('@cucumber/cucumber');
+const { Given, When, Then } = require('@wdio/cucumber-framework');
 const { expect } = require('@wdio/globals');
 const loginPage = require('../../test/pages/LoginPage');
 
